0d81a777dcac52791cd71c471dd81a9ae1c8b838
Milan Misak <admin@milanmisak.com>
2019-10-06 14:22:12 +0100

    Disable add button when the new item name is empty

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,11 @@
 
 	let newItem = '';
 
+	$: newItemTrimmed = newItem.trim();
+
 	const addTodo = () => {
-		console.log(newItem);
-		if (newItem.trim().length) {
-			todos = [{name: newItem.trim(), done: false}, ...todos];
-			newItem = '';
-		}
+		todos = [{name: newItemTrimmed, done: false}, ...todos];
+		newItem = '';
 	};
 
 	const removeTodo = i => todos = [...todos.slice(0, i), ...todos.slice(i + 1)];
@@ -27,7 +26,7 @@
 </style>
 
 <form on:submit|preventDefault>
-	<input bind:value={newItem} type="text" placeholder="New item" autofocus /> <button on:click={addTodo}>Add</button>
+	<input bind:value={newItem} type="text" placeholder="New item" autofocus /> <button disabled={!newItemTrimmed.length} on:click={addTodo}>Add</button>
 </form>
 
 
