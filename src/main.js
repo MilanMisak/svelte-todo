ac936df7322d87af6fc98ca31a4ae19e0a3eef39
Milan Misak <admin@milanmisak.com>
2019-11-10 14:32:01 +0000

    Removed hardcoded todos

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import App from './App.svelte';
 
-const todos = [
-    {
-        id: 0,
-        name: 'Water plants',
-        done: false
-    },
-    {
-        id: 1,
-        name: 'Take rubbish out',
-        done: true
-    }
-];
+const todos = [];
 
 const app = new App({
     target: document.body,
